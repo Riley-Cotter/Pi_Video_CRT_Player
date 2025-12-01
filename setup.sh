04a5a6be7bd32d4aa3d4a96dbebee7192857475b
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-11-30 18:10:02 -0600

    Remove Mosquitto installation and update script path
    Removed installation of Mosquitto and its clients, and updated the path for setting executable permissions on shell scripts.

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+sudo apt-get install -y vlc
+sudo sed -i 's/geteuid/getppid/' /usr/bin/vlc
+sudo apt-get install -y fbi
+sudo apt install -y python3-paho-mqtt
+sudo apt install -y python3-vlc
+
+#Set shell scripts code to executable
+chmod +x /home/ri/Pi_Video_CRT_Player/*.sh
+
+echo "[✔] Setup Complete"
