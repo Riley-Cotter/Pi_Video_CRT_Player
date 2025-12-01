274b93c586f229d8839bddf8034f1d8030242d10
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-11-30 21:25:52 -0600

    Fix syntax error in video output option

--- video_player.py
+++ video_player.py
@@ -49,7 +49,7 @@ def play_video(video_path):
             '--fullscreen',
             '--no-video-title-show',
             '--aout=alsa',
-            '--vout=drm_vout",
+            '--vout=drm_vout',
             video_path
         ], check=True)
         
