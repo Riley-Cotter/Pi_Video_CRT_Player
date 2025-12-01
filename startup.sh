7836479b84453eda68dd9d74cfe571c61dfeca00
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-11-30 22:21:25 -0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,9 +8,9 @@ echo -e "Start USB MOUNT" >> "$LOG_FILE"
 /home/ri/Pi_Video_CRT_Player/mount_usb.sh >> "$LOG_FILE" 2>&1 &
 
 # Start background image script
-#/bin/sleep 3
-#echo -e "Start Background Image" >> "$LOG_FILE"
-#/usr/bin/python3 /home/ri/Pi_Video_CRT_Player/background_image.py >> "$LOG_FILE" 2>&1 &
+/bin/sleep 3
+echo -e "Start Background Image" >> "$LOG_FILE"
+/usr/bin/python3 /home/ri/Pi_Video_CRT_Player/background_image.py >> "$LOG_FILE" 2>&1 &
 
 # Pull latest repo update
 /bin/sleep 3
