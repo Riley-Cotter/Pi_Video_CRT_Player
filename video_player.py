e84b91d30267e4aa932cb5809164a85df5b982d0
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-11-30 21:26:57 -0600

    Change video output option from drm_vout to fb

--- video_player.py
+++ video_player.py
@@ -49,7 +49,7 @@ def play_video(video_path):
             '--fullscreen',
             '--no-video-title-show',
             '--aout=alsa',
-            '--vout=drm_vout',
+            "--vout", "fb",
             video_path
         ], check=True)
         
